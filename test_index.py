b349517af2a55eaeb23680bc75bfb067da225517
Shaquille004 <shaquillesma04@gmail.com>
2024-12-06 21:05:15 -0400

    prueba 4

--- test_index.py
+++ test_index.py
@@ -1,29 +1,10 @@
 import unittest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 
-class TestHelloWorld(unittest.TestCase):
-
-    def setUp(self):
-        options = Options()
-        options.add_argument("--headless")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        self.driver = webdriver.Chrome(options=options)
-
-    def test_title(self):
-        self.driver.get("http://localhost:9000:80")
-        self.assertEqual(self.driver.title, "Hola Mundo")
-
-    def test_heading(self):
-        self.driver.get("http://localhost:9000:80")
-        heading = self.driver.find_element(By.TAG_NAME, "h1")
-        self.assertEqual(heading.text.strip(), "Hola Mundo")
-
-    def tearDown(self):
-        self.driver.quit()
+class TestIndexPage(unittest.TestCase):
+    def test_header(self):
+        with open("index.html", "r", encoding="utf-8") as f:
+            content = f.read()
+            self.assertIn("<h1>Hola Mundo</h1>", content, "El encabezado no contiene 'Hola Mundo'")
 
 if __name__ == "__main__":
     unittest.main()
-
